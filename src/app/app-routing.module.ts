73696b6ae2b368dc9edbaffcae12086d9fae2e02
Rahul Singh <singh.rahul.net@gmail.com>
2018-09-16 17:41:22 +1000

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {UsersComponent} from './users/users.component';
+import {PostsComponent} from './posts/posts.component';
+import {DetailsComponent} from './details/details.component';
+import { compileComponentFromMetadata } from '@angular/compiler';
+
+
+const routes: Routes = [
+{
+  path:'',
+  component:UsersComponent
+},
+
+{
+  path: 'details/:id',
+  component:DetailsComponent
+},
+{
+path:'posts',
+component: PostsComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
